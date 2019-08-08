4c473fab2841a0780bae17617a7a83a5dc5ae5d2
admin <admin@ALC-45T6LQ1.usersad.everis.int>
2019-08-08 15:18:06 +0200

    conflict

--- BecaUlab0708/src/com/es/eoi/Ahorcado/Juego.java
+++ BecaUlab0708/src/com/es/eoi/Ahorcado/Juego.java
@@ -1,5 +1,6 @@
 package com.es.eoi.Ahorcado;
 
+import java.io.Console;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,57 +19,155 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Juego {
 
 	private static List<Character> usedC;
-	private static char[] chars;
+	private static char[] offuscatedWord;
+	private static char[] word;
+	private static BufferedReader userInput;
 
 	// declaramos el tamaño de palabra y las lineas del dibujo
 	private static final int wordSize = 6;
 	private static final int numLines = 9;
+	private static final int lives = 5;
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 
-		// new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-
+		System.out.println("Pulse Intro para comenzar a jugar.");
 		game();
 
 	}
 
-	public static void game() throws IOException {
+	public static void game() throws IOException, InterruptedException {
 
 		// creamos el buffer de palabra y lo rellenamos con '_'
-		chars = new char[wordSize];
-		for (int i = 0; i < chars.length; i++) {
-			chars[i] = '_';
+		offuscatedWord = new char[wordSize];
+		for (int i = 0; i < offuscatedWord.length; i++) {
+			offuscatedWord[i] = '_';
 		}
 
 		// creamos una lista de los caracteres usados
 		usedC = new ArrayList<Character>();
 
 		// leemos todas las palabras del diccionario
-		File f = new File("words.txt");
+		File f = new File("C:\\Users\\Cauntu\\Desktop\\Repo\\alumnos\\BecaUlab0708\\resources\\words.txt");
 		List<String> words = Files.readAllLines(f.toPath());
 
 		// elegimos un numero al azar
 		int numPalabra = ThreadLocalRandom.current().nextInt(0, words.size());
 
 		// escogemos la palabra de dicho numero
-		char[] word = words.get(numPalabra).toCharArray();
+		word = words.get(numPalabra).toCharArray();
 
 		// nos preparamos para dibujar al ahorcado y recibir letras
-		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-		FileReader state0 = new FileReader("hangman0.txt");
-		FileReader state1 = new FileReader("hangman1.txt");
-		FileReader state2 = new FileReader("hangman2.txt");
-		FileReader state3 = new FileReader("hangman3.txt");
-		FileReader state4 = new FileReader("hangman4.txt");
-		FileReader state5 = new FileReader("hangman_full.txt");
+		userInput = new BufferedReader(new InputStreamReader(System.in));
+		userInput.read();
 
-		drawHangMan(state0);
-		writeWordState();
-		writeUsed();
+		for (int i = 0; i <= lives;) {
+
+			if (!checkVictory()) {
+
+				if (i < lives) {
+
+					drawHangMan(i);
+					writeWordState();
+					writeUsed();
+					Character charIn;
+
+					do {
+						System.out.println();
+						System.out.print("Introduzca una letra: ");
+						charIn = Character.toUpperCase(askUserInput());
+					} while (!validateUserInput(charIn));
+
+					if (!checkMatch(charIn)) {
+
+						i++;
+
+					}
+
+				} else {
+
+					drawHangMan(i);
+					writeWordState();
+					writeUsed();
+
+					System.out.println();
+					System.out.println("Pulse Intro para intentarlo de nuevo.");
+					System.in.read();
+					game();
+				}
+
+				// comprobamos si hemos perdido o ganado
+				if (checkVictory()) {
+
+					drawHangMan(i);
+					writeWordState();
+					writeUsed();
+					i = 5;
+
+					System.out.println();
+					System.out.println("Enhorabuena, has ganado.");
+					System.out.println("Pulse Intro para intentarlo de nuevo.");
+					System.in.read();
+					game();
+				}
+			}
+		}
+		game();
 
 	}
 
-	public static void drawHangMan(FileReader fr) throws IOException {
+	public static void drawHangMan(int state) throws IOException {
+
+		FileReader fr = null;
+		String statePath;
+
+		switch (state) {
+
+		case 0:
+
+			statePath = new File("resources/ascii/hangman0.txt").getAbsolutePath();
+			fr = new FileReader(statePath);
+
+			break;
+
+		case 1:
+			
+			statePath = new File("resources/ascii/hangman1.txt").getAbsolutePath();
+			fr = new FileReader(statePath);
+			
+			break;
+
+		case 2:
+			
+			statePath = new File("resources/ascii/hangman2.txt").getAbsolutePath();
+			fr = new FileReader(statePath);
+			
+			break;
+
+		case 3:
+			
+			statePath = new File("resources/ascii/hangman3.txt").getAbsolutePath();
+			fr = new FileReader(statePath);
+			
+			break;
+
+		case 4:
+			
+			statePath = new File("resources/ascii/hangman4.txt").getAbsolutePath();
+			fr = new FileReader(statePath);
+			
+			break;
+
+		case 5:
+			
+			statePath = new File("resources/ascii/hangman_full.txt").getAbsolutePath();
+			fr = new FileReader(statePath);
+			
+			break;
+
+		default:
+
+			break;
+		}
 
 		BufferedReader fileInput = new BufferedReader(fr);
 
@@ -83,10 +182,10 @@ public class Juego {
 
 		System.out.println();
 		System.out.print("(");
-		for (int i = 0; i < chars.length; i++) {
+		for (int i = 0; i < offuscatedWord.length; i++) {
 
-			System.out.print(chars[i]);
-			if (i != chars.length - 1) {
+			System.out.print(offuscatedWord[i]);
+			if (i != offuscatedWord.length - 1) {
 				System.out.print(" ");
 			}
 
@@ -98,12 +197,84 @@ public class Juego {
 
 		System.out.print("	");
 		System.out.print("Usados: [");
-		for (Character c : usedC) {
-			System.out.print(c + " ");
+
+		for (int i = 0; i < usedC.size(); i++) {
+
+			System.out.print(usedC.get(i));
+
+			if (i != usedC.size() - 1) {
+
+				System.out.print(" ");
+			}
 		}
 
 		// mas formato
 		System.out.print("]");
+
+	}
+
+	public static Character askUserInput() throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		return (char) br.read();
+
+	}
+
+	public static Boolean validateUserInput(char input) {
+
+		if (input == '\n' || input == '\r') {
+			return false;
+		}
+
+		// descartamos el caracter si ya se usó
+		for (Character c : usedC) {
+
+			if (c == input) {
+				return false;
+			}
+		}
+
+		// añadimos el caracter a usados
+		usedC.add(input);
+
+		return true;
+
+	}
+
+	public static Boolean checkMatch(char input) {
+
+		if (input == '\n' || input == '\r') {
+			return true;
+		}
+
+		Boolean match = false;
+
+		// comprobamos si el caracter está en la palabra
+		for (int i = 0; i < word.length; i++) {
+
+			if (input == word[i]) {
+
+				offuscatedWord[i] = input;
+				match = true;
+			}
+
+		}
+
+		return match;
+
+	}
+
+	public static Boolean checkVictory() {
+
+		for (int j = 0; j < offuscatedWord.length; j++) {
+
+			if (offuscatedWord[j] == '_') {
+				return false;
+			}
+
+		}
+		return true;
 
 	}
 

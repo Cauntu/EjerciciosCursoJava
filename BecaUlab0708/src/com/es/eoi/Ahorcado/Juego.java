61758ba25ec7695e438beb2bb06c43e4a2f3ebe5
admin <admin@ALC-45T6LQ1.usersad.everis.int>
2019-08-08 15:46:43 +0200

    fixed yet another abs path to rel
    updated ascii renders

--- BecaUlab0708/src/com/es/eoi/Ahorcado/Juego.java
+++ BecaUlab0708/src/com/es/eoi/Ahorcado/Juego.java
@@ -47,7 +47,7 @@ public class Juego {
 		usedC = new ArrayList<Character>();
 
 		// leemos todas las palabras del diccionario
-		File f = new File("C:\\Users\\Cauntu\\Desktop\\Repo\\alumnos\\BecaUlab0708\\resources\\words.txt");
+		File f = new File("resources/words.txt");
 		List<String> words = Files.readAllLines(f.toPath());
 
 		// elegimos un numero al azar
